f7b0b79dac87d06fd352d8c402b1ec8d494e2a15
Jordan gillispie <jordangillispie@gmail.com>
2022-11-09 23:16:42 -0700

    added prob desc & func body

---
+++ questionOne/twoSum.js
@@ -0,0 +1,16 @@
+// Leetcode question 1 Add two numbers
+// 
+// Input: [nums] & target val
+// 
+// expected output: two indicies that add up to target val
+// 
+// O.O.B: only one solution and cant use elem twice
+
+// Test cases to try:
+// [2,7,11,15]
+// [3,2,4]
+// [3,3]
+
+let twoSum = function (nums, target) {
+
+};
