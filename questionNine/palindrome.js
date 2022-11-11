e82cc4f67aec4f9cdd61217fff5db387e9cca54e
Jordan gillispie <jordangillispie@gmail.com>
2022-11-10 18:52:57 -0700

    added prob desc & func body

---
+++ questionNine/palindrome.js
@@ -0,0 +1,17 @@
+// Leetcode question 9 is given val a palindrome
+// 
+// Input: [nums] in form of arr
+// 
+// expected output: T or F (true if isPalindrome)
+// 
+// O.O.B: -2^31 <= x <= 2^31 - 1
+
+// Test cases to try:
+// [3,2,1]
+// [1,2,3]
+// [1,2,1]
+// [-1,2,1];
+
+var isPalindrome = function (x) {
+
+}
