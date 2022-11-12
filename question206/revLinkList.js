d4097464469b9a637fac13910c567dfa5c966ddc
Jordan gillispie <jordangillispie@gmail.com>
2022-11-11 23:52:26 -0700

    added logic to facilitate reveral of LL

--- question206/revLinkList.js
+++ question206/revLinkList.js
@@ -19,7 +19,14 @@ var reverseList = function (head) {
     let prev = null;
     let current = head;
     while (current) {
-
+        // next var will help us continue through loop
+        let next = current.next;
+        // redefine what the next node for current traversal (this is where LL flips pointer)
+        current.next = prev;
+        // reasign prev to current so that on next itteration we have current pointing to the proper node
+        prev = current;
+        // use next var to continue through loop
+        current = next;
     }
     // return new head of LL which will end up being prev
     return prev;
