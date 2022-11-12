9d179d8f248578dc468185b37e3ce75a8bac9aac
Jordan gillispie <jordangillispie@gmail.com>
2022-11-11 23:41:07 -0700

    added prob desc & func body

---
+++ question206/revLinkList.js
@@ -0,0 +1,19 @@
+// Leetcode question 206 Reverse Linked List
+// 
+// Input: head of single linked list
+// 
+// expected output: return reversed linked list
+// 
+// O.O.B: The number of nodes in the list is the range [0, 5000].
+
+// Start by creating a LL 
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+
+
+var reverseList = function (head) {
+
+};
