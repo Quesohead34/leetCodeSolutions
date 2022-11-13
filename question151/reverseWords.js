3ccc661a8d101c8b7a7527fb802555201cc18922
Jordan gillispie <jordangillispie@gmail.com>
2022-11-13 16:59:46 -0700

    added prob desc & func body

---
+++ question151/reverseWords.js
@@ -0,0 +1,20 @@
+// Leetcode question 151 reverse words in a string
+// 
+// Input: "string" 
+// 
+// expected output: "reversed string" 
+// 
+// O.O.B: 1 <= s.length <= 104 
+// s contains English letters (upper-case and lower-case), digits, and spaces ' '
+// There is at least one word in s.
+
+// Test cases to try:
+// let s = "the sky is blue";
+// let s = "  hello world  ";
+// let s = "a good   example";
+
+var reverseWords = function (s) {
+
+};
+
+reverseWords(s);
