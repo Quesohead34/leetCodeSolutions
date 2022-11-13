3e3a86b0fc7fbd57758be566b8cf5a59d5f65c31
Jordan gillispie <jordangillispie@gmail.com>
2022-11-13 13:31:30 -0700

    added prob desc & func body

---
+++ question136/singleNum.js
@@ -0,0 +1,18 @@
+// Leetcode question 136 single number
+// 
+// Input: [nums] 
+// 
+// expected output: return the onlu number in given arr that doesnt repeat
+// 
+// O.O.B: 1 <= nums.length <= 3 * 10^4 
+// -3 * 104 <= nums[i] <= 3 * 104
+
+// Test cases to try:
+// [2,2,1]
+// [4,1,2,1,2]
+// [1]
+
+
+var singleNumber = function (nums) {
+
+};
