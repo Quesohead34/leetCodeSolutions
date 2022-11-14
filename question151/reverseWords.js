18b08e13ebb358a9151c64687f062b425703569d
Jordan gillispie <jordangillispie@gmail.com>
2022-11-13 19:06:49 -0700

    added body of code for str reverse logic

--- question151/reverseWords.js
+++ question151/reverseWords.js
@@ -13,8 +13,15 @@
 // let s = "  hello world  ";
 // let s = "a good   example";
 
+// 
+// to solve this I will use a 4 chained methods which I will describe bellow
+// the split will split the string where there is a space
+// the filter method will return a shallow array that removes unneccesary spaces
+// the reverse will then take that array and reverse the order of elems
+// the join will take each element and join to a str with the space as the seperator 
+// 
 var reverseWords = function (s) {
-
+    return s.split(" ").filter((word) => word.length > 0).reverse().join(' ');
 };
 
 reverseWords(s);
