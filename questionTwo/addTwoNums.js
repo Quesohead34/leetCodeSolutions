12eba7e783201f5ce80ada70569616f75c8c8bc8
Jordan gillispie <jordangillispie@gmail.com>
2022-11-21 20:09:55 -0700

    added prob desc & func body

---
+++ questionTwo/addTwoNums.js
@@ -0,0 +1,14 @@
+// Leetcode question 2 Add two numbers
+// 
+// Input: two linked lists (arrays) thatre reversed
+// 
+// expected output: return the sum of both linked lists
+// 
+// O.O.B: each node in LL will have only 1 character & all non neg nums
+
+// start by defining a func with two inputs
+var addTwoNumbers = function (l1, l2) {
+
+};
+
+console.log(addTwoNumbers([3, 2, 1], [6, 5, 4]));
