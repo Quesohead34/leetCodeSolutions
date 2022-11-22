796af57d465b44167196218f689c3f25f8ddcdd9
Jordan gillispie <jordangillispie@gmail.com>
2022-11-21 20:14:26 -0700

    used built in mutations to rev & add

--- questionTwo/addTwoNums.js
+++ questionTwo/addTwoNums.js
@@ -8,7 +8,13 @@
 
 // start by defining a func with two inputs
 var addTwoNumbers = function (l1, l2) {
+    // take two inputs and reverse them & join together to get proper num
+    let rL1 = l1.reverse().join('');
+    let rL2 = l2.reverse().join('');
+    // convert reversed strings into a int to add (handles edge case for carry num)
+    let finalArr = parseInt(rL1) + parseInt(rL2);
+    return finalArr.toString().split('');
 
 };
 
-console.log(addTwoNumbers([3, 2, 1], [6, 5, 4]));
+console.log(addTwoNumbers([9, 2, 1], [6, 5, 4]));
